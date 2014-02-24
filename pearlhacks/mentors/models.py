89d650a7a34dbc8e3f1e08757d1a3e2960e2326f
mclawges22 <mclawges22@gmail.com>
2014-02-23 22:33:43 -0500

    getting errors when trying to loaddata from mentors.json so stopping until class tomorrow

--- pearlhacks/mentors/models.py
+++ pearlhacks/mentors/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Mentor(models.Model):
-    firstName = models.CharField(max_length=20)
-    lastName = models.CharField(max_length=20)
+    nameFirst = models.CharField(max_length=20)
+    nameLast = models.CharField(max_length=20)
     company = models.CharField(max_length=30)
     companyUrl = models.CharField(max_length=50)
     twitter = models.CharField(unique=True, max_length=50)
     linkedIn = models.CharField(unique=True, max_length=50)
     website = models.CharField(max_length=50)
     linkOther = models.CharField(max_length=50)
-    # tech experience ??
+    # techExp = models.CharField(max_length=1000)
     language = models.CharField(max_length=20)
     languageUrl = models.CharField(max_length=50)
     almaMater = models.CharField(max_length=50)
@@ -21,9 +21,9 @@ class Mentor(models.Model):
     afraid = models.CharField(unique=True, max_length=500)
     proud = models.CharField(unique=True, max_length=500)
     about = models.CharField(unique=True, max_length=500)
-    # hobbies ??
+    # hobbies = models.CharField(max_length=1000)
 
     # pub_date = models.DateTimeField('date published')
 
     class Meta(object):
-		ordering = ('firstName', 'lastName', 'company')
+		ordering = ('nameFirst', 'nameLast', 'company')
